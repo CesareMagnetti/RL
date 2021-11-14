7ebdba74f4882fcccc6f014ebf2a49e1cb39f9ff
cesare <cesare.magnetti98@gmail.com>
2021-11-14 17:45:14 +0000

    moved to src folder

---
+++ src/RL/__init__.py
@@ -0,0 +1,8 @@
+"""
+RL
+
+A library with common RL routines intended for personal future projects which will require those routines. Anyone is wellcome to use it, if so please cite :)
+"""
+
+__version__ = "0.1.0"
+__author__ = "Cesare Magnetti"
